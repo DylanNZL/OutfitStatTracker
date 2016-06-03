eddf3e0b09da729f1fc932291b9dfa9e46e1bca0
Dylan Cross <dylancross@me.com>
2016-06-03 17:20:51 +1200

    Subscribe to events from the object

--- planetsideWebSockets.js
+++ planetsideWebSockets.js
@@ -12,21 +12,20 @@ var WebSocket = require('ws'),
 
     // Global Variables
 
-
-
-/*
-function createStream() {
+function createStream(Outfit) {
     var ws = new WebSocket('wss://push.planetside2.com/streaming?environment=ps2&service-id=s:' + api_key.KEY);
     ws.on('open', function open() {
         console.log('stream opened');
-        foreach (Char in Outfit) {
-            ws.send('{"service":"event","action":"subscribe","characters":["' + Char + '"],"eventNames":["Death"]}');
-        }
-        subscribe(ws);
+        Outfit.Members.forEach(function(Members) {
+            //subscribe to each characters kill events
+            ws.send('{"service":"event","action":"subscribe","characters":["' + Members.CharacterID + '"],"eventNames":["Death"]}');
+        });
+        // Subscribe to Briggs captures
+        ws.send('{"service":"event","action":"subscribe","worlds":["25"],"eventNames":["FacilityControl"]}');
     });
     ws.on('message', function (data) {
-
+        //do stuff
     });
-    captures = 0;
 }
-*/
+
+exports.createStream = createStream;
