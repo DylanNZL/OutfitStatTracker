9c431d4e335d21e270d28313a9804a88f47dc12e
Dylan Cross <dylancross@me.com>
2016-06-07 15:59:44 +1200

    Tidied up code

--- Outfit.js
+++ Outfit.js
@@ -2,10 +2,10 @@
  * Created by dylancross on 3/06/16.
  */
     // Required Files
-var api_key = require('./api_key.js');
+var api_key     = require('./api_key.js');
     // Required Node Modules
-var prequest = require('prequest'),
-    Q = require('q');
+var prequest    = require('prequest'),
+    Q           = require('q');
     //Global Variables
 
 function fetchTrackingOutfit(outfitTag) {
