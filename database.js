010e0c8e5fd19ea53ca348e418ac2ad84c07b2ee
Dylan Cross <dylancross@me.com>
2016-06-28 17:48:31 +1200

    Added calls to the outfit DB

--- database.js
+++ database.js
@@ -4,7 +4,9 @@
 // Required Files
 var bookshelf     = require('./bookshelf.js');
 // Required Modules
-var knex          = require('knex');
+//var knex          = require('knex');
+
+/* ======================================== Character Queries ======================================== */
 
 // Insert a tracked outfit kill into the appropriate databases
 function addTrackedKill(mTime, mKiller, mWeapon, mLoadout, mLoser, mLoserLoadout, mIsHeadshot) {
@@ -87,6 +89,76 @@ function doesCharacterExist (char_id) {
     });
 }
 
+/* ========================================== Outfit Queries ========================================== */
+
+// Adds an outfit to the database
+function addOutfit(mID, mName, mAlias, mFaction, mMembers) {
+    var obj = {
+        outfit_id : mID,
+        name : mName,
+        alias : mAlias,
+        kills : 0,
+        deaths : 0,
+        faction : mFaction,
+        members : mMembers,
+        created : Date.now(),
+        updated : Date.now()
+    };
+    bookshelf.knex('outfits').insert(obj).then(function (data) {
+
+    }).catch(function (err) {
+        console.error(err)
+    })
+}
+
+// Update Outfit Kills
+function updateOutfitKills(mID) {
+    bookshelf.knex('outfits').where('outfit_id', mID).select('kills').then(function (data) {
+        var dat = data[0].kills;
+        dat++;
+        bookshelf.knex('outfits').where('outfit_id', mID).update({ kills : dat, updated : Date.now() }).then(function (d) {
+
+        }).catch(function (err) {
+            console.error(err);
+        })
+    }).catch(function (err) {
+        console.error(err);
+    })
+}
+
+// Update Outfit Deaths
+function updateOutfitDeaths(mID) {
+    bookshelf.knex('outfits').where('outfit_id', mID).select('deaths').then(function (data) {
+        var dat = data[0].deaths;
+        dat++;
+        bookshelf.knex('outfits').where('outfit_id', mID).update({ deaths : dat }).then(function (d) {
+
+        }).catch(function (err) {
+            console.error(err);
+        })
+    }).catch(function (err) {
+        console.error(err);
+    })
+}
+
+// Check if an outfit exists in the db
+function doesOutfitExist(mID, callback) {
+    bookshelf.knex('outfits').where('outfit_id', mID).select('outfit_id').then(function (data) {
+        if ((data) && (data.length > 0)) {
+            console.log(data);
+            callback(true);
+        } else {
+            console.error(data);
+            callback(false);
+        }
+    }).catch(function (err) {
+        console.error(err);
+        callback(false);
+    })
+}
+
+/* =========================================== Base Queries =========================================== */
+
 // Adds a base capture into the DB that stores them.
 function addBaseCapture(mtime, mID, mName, mPrevFaction, mNewFaction, mOutfitID) {
     var obj = {
@@ -136,13 +208,45 @@ function updateCapturesOfABase(mID) {
     })
 }
 
-addBaseCapture("123456", "123", "testBase", "1", "2", "789789");
+// Checks Base db to see if the base is there.
+function doesBaseExist(mID) {
+    bookshelf.knex('bases').where('base_id', mID).select('base_id').then(function (data) {
+        if ((data) && (data.length > 0)) {
+            console.log(data);
+            return true;
+        } else {
+            console.error(data);
+            return false;
+        }
+    }).catch(function (err) {
+        console.log(err);
+        return false;
+    })
+}
 
+/* ========================================== Function Tests ========================================== */
+
+//  Outfit Function Tests
+//  addOutfit('123456', 'test Outfit', 'test', '1', '69');
+//  updateOutfitKills(123456);
+//  updateOutfitDeaths("123456");
+//  doesOutfitExist(123456, function (result) {
+//      console.log("we made it");
+//  });
+
+// Character functions
 exports.addTrackedKill              = addTrackedKill;
 exports.addTrackedDeath             = addTrackedDeath;
 exports.updateDeathsOfACharacter    = updateDeathsOfACharacter;
 exports.updateKillsOfACharacter     = updateKillsOfACharacter;
 exports.doesCharacterExist          = doesCharacterExist;
+// Outfit Functions
+exports.addOutfit                   = addOutfit;
+exports.updateOutfitKills           = updateOutfitKills;
+exports.updateOutfitDeaths          = updateOutfitDeaths;
+exports.doesOutfitExist             = doesOutfitExist;
+// Base Functions
 exports.addBaseCapture              = addBaseCapture;
 exports.addNewBaseCaptureToCaptures = addNewBaseCaptureToCaptures;
 exports.updateCapturesOfABase       = updateCapturesOfABase;
+exports.doesBaseExist               = doesBaseExist;
